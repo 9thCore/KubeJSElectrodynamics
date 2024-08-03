909f9d181b8c5d2147528440a1da09671566007b
9thCore <38700349+9thCore@users.noreply.github.com>
2024-08-03 12:57:28 +0300

    got mixins working i think

--- src/main/java/com/core/kubejselectrodynamics/mixin/FluidIngredientMixin.java
+++ src/main/java/com/core/kubejselectrodynamics/mixin/FluidIngredientMixin.java
@@ -1,5 +1,6 @@
-package com.core.kubejselectrodynamics.mixin.fluidingredient;
+package com.core.kubejselectrodynamics.mixin;
 
+import com.core.kubejselectrodynamics.util.FluidIngredientInterface;
 import electrodynamics.common.recipe.recipeutils.FluidIngredient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
--- src/main/java/com/core/kubejselectrodynamics/util/FluidIngredientInterface.java
+++ src/main/java/com/core/kubejselectrodynamics/util/FluidIngredientInterface.java
@@ -1,4 +1,4 @@
-package com.core.kubejselectrodynamics.mixin.fluidingredient;
+package com.core.kubejselectrodynamics.util;
 
 public interface FluidIngredientInterface {
     int kjsElectroMixin$getAmount();
