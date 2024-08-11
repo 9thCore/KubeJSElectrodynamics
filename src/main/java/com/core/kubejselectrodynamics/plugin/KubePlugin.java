a9abd93d5a5977835546177d88a303a10bf0563a
9thCore <38700349+9thCore@users.noreply.github.com>
2024-08-11 21:20:13 +0300

    add blastcraft support
    so much easier than custom battery boxes

--- src/main/java/com/core/kubejselectrodynamics/plugin/KubePlugin.java
+++ src/main/java/com/core/kubejselectrodynamics/plugin/KubePlugin.java
@@ -11,6 +11,7 @@ import com.core.kubejselectrodynamics.plugin.recipe.schema.ItemToItem;
 import com.core.kubejselectrodynamics.plugin.recipe.schema.gas.ElectroGasWrapper;
 import com.core.kubejselectrodynamics.plugin.recipe.schema.gas.GasBuilder;
 import com.core.kubejselectrodynamics.util.ElectroFluidWrapper;
+import dev.architectury.platform.Platform;
 import dev.latvian.mods.kubejs.KubeJSPlugin;
 import dev.latvian.mods.kubejs.recipe.schema.RecipeSchema;
 import dev.latvian.mods.kubejs.recipe.schema.RegisterRecipeSchemasEvent;
@@ -19,12 +20,12 @@ import dev.latvian.mods.kubejs.script.BindingsEvent;
 import net.minecraft.resources.ResourceLocation;
 
 public class KubePlugin extends KubeJSPlugin {
-    public ResourceLocation GetRecipeType(String type) {
-        return new ResourceLocation("electrodynamics", type);
+    public ResourceLocation GetRecipeType(RecipeHolder holder, String type) {
+        return new ResourceLocation(holder.mod, type);
     }
 
-    public void register(RegisterRecipeSchemasEvent event, String name, RecipeSchema schema) {
-        event.register(GetRecipeType(name + "_recipe"), schema);
+    public void register(RegisterRecipeSchemasEvent event, RecipeHolder holder, String name, RecipeSchema schema) {
+        event.register(GetRecipeType(holder, name + "_recipe"), schema);
     }
 
     @Override
@@ -43,18 +44,24 @@ public class KubePlugin extends KubeJSPlugin {
 
     @Override
     public void registerRecipeSchemas(RegisterRecipeSchemasEvent event) {
-       register(event, "energized_alloyer", ItemToItem.ALLOYER_SCHEMA);
-        register(event, "lathe", ItemToItem.SCHEMA);
-        register(event, "mineral_crusher", ItemToItem.SCHEMA);
-        register(event, "mineral_grinder", ItemToItem.SCHEMA);
-        register(event, "oxidation_furnace", ItemToItem.OXIDIZER_FURNACE_SCHEMA);
-        register(event, "reinforced_alloyer", ItemToItem.ALLOYER_SCHEMA);
-        register(event, "wire_mill", ItemToItem.SCHEMA);
-        register(event, "chemical_crystallizer", FluidToItem.SCHEMA);
-        register(event, "chemical_mixer", FluidItemToFluid.SCHEMA);
-        register(event, "fermentation_plant", FluidItemToFluid.SCHEMA);
-        register(event, "mineral_washer", FluidItemToFluid.SCHEMA);
-        register(event, "electrolytic_separator", FluidToGas.SCHEMA);
+        // Electrodynamics
+        register(event, RecipeHolder.ELECTRODYNAMICS, "energized_alloyer", ItemToItem.ALLOYER_SCHEMA);
+        register(event, RecipeHolder.ELECTRODYNAMICS, "lathe", ItemToItem.SCHEMA);
+        register(event, RecipeHolder.ELECTRODYNAMICS, "mineral_crusher", ItemToItem.SCHEMA);
+        register(event, RecipeHolder.ELECTRODYNAMICS, "mineral_grinder", ItemToItem.SCHEMA);
+        register(event, RecipeHolder.ELECTRODYNAMICS, "oxidation_furnace", ItemToItem.OXIDIZER_FURNACE_SCHEMA);
+        register(event, RecipeHolder.ELECTRODYNAMICS, "reinforced_alloyer", ItemToItem.ALLOYER_SCHEMA);
+        register(event, RecipeHolder.ELECTRODYNAMICS, "wire_mill", ItemToItem.SCHEMA);
+        register(event, RecipeHolder.ELECTRODYNAMICS, "chemical_crystallizer", FluidToItem.SCHEMA);
+        register(event, RecipeHolder.ELECTRODYNAMICS, "chemical_mixer", FluidItemToFluid.SCHEMA);
+        register(event, RecipeHolder.ELECTRODYNAMICS, "fermentation_plant", FluidItemToFluid.SCHEMA);
+        register(event, RecipeHolder.ELECTRODYNAMICS, "mineral_washer", FluidItemToFluid.SCHEMA);
+        register(event, RecipeHolder.ELECTRODYNAMICS, "electrolytic_separator", FluidToGas.SCHEMA);
+
+        // Blastcraft
+        if (Platform.isModLoaded("blastcraft")) {
+            register(event, RecipeHolder.BLASTCRAFT, "blast_compressor", ItemToItem.SCHEMA);
+        }
     }
 
     @Override
@@ -62,5 +69,15 @@ public class KubePlugin extends KubeJSPlugin {
         super.registerBindings(event);
         event.add("ElectrodynamicsFluid", ElectroFluidWrapper.class);
         event.add("ElectrodynamicsGas", ElectroGasWrapper.class);
+    }
+
+    enum RecipeHolder {
+        ELECTRODYNAMICS("electrodynamics"),
+        BLASTCRAFT("blastcraft");
+
+        public final String mod;
+        RecipeHolder(String name) {
+            this.mod = name;
+        }
     }
 }
