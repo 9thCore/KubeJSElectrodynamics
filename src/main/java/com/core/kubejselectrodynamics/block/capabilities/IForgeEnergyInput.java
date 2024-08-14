bb6af382d4cc2d079db7712decc61ebf5fb1021b
9thCore <38700349+9thCore@users.noreply.github.com>
2024-08-14 12:44:45 +0300

    add FE i/o interfaces

---
+++ src/main/java/com/core/kubejselectrodynamics/block/capabilities/IForgeEnergyInput.java
@@ -0,0 +1,33 @@
+package com.core.kubejselectrodynamics.block.capabilities;
+
+import dev.latvian.mods.kubejs.block.BlockBuilder;
+import dev.latvian.mods.kubejs.typings.Info;
+import dev.latvian.mods.rhino.util.HideFromJS;
+import net.minecraft.core.Direction;
+
+import java.util.Arrays;
+import java.util.EnumSet;
+
+public interface IForgeEnergyInput<T extends BlockBuilder> extends ISelfAccessor<T> {
+    @Info("Sets the machine's FE input faces.\nDirection.ALL is not supported, use #allFEInput() instead.")
+    default T feInputs(Direction[] directions) {
+        getFEInputSet().clear();
+        getFEInputSet().addAll(Arrays.asList(directions));
+        return getThis();
+    }
+    @Info("Sets the machine's FE input face.\nDirection.ALL is not supported, use #allFEInput() instead.")
+    default T feInput(Direction direction) {
+        return feInputs(new Direction[] {direction});
+    }
+    @Info("Allows FE output through every face.")
+    default T allFEInput() {
+        getFEInputSet().clear();
+        return getThis();
+    }
+    @HideFromJS
+    default Direction[] getFEInput() {
+        return getFEInputSet().toArray(new Direction[0]);
+    }
+    @HideFromJS
+    EnumSet<Direction> getFEInputSet();
+}
---
+++ src/main/java/com/core/kubejselectrodynamics/block/capabilities/IForgeEnergyOutput.java
@@ -0,0 +1,33 @@
+package com.core.kubejselectrodynamics.block.capabilities;
+
+import dev.latvian.mods.kubejs.block.BlockBuilder;
+import dev.latvian.mods.kubejs.typings.Info;
+import dev.latvian.mods.rhino.util.HideFromJS;
+import net.minecraft.core.Direction;
+
+import java.util.Arrays;
+import java.util.EnumSet;
+
+public interface IForgeEnergyOutput<T extends BlockBuilder> extends ISelfAccessor<T> {
+    @Info("Sets the machine's FE output faces.\nDirection.ALL is not supported, use #allFEOutput() instead.")
+    default T feOutputs(Direction[] directions) {
+        getFEOutputSet().clear();
+        getFEOutputSet().addAll(Arrays.asList(directions));
+        return getThis();
+    }
+    @Info("Sets the machine's FE output face.\nDirection.ALL is not supported, use #allFEOutput() instead.")
+    default T feOutput(Direction direction) {
+        return feOutputs(new Direction[] {direction});
+    }
+    @Info("Allows FE output through every face.")
+    default T allFEOutput() {
+        getFEOutputSet().clear();
+        return getThis();
+    }
+    @HideFromJS
+    default Direction[] getFEOutput() {
+        return getFEOutputSet().toArray(new Direction[0]);
+    }
+    @HideFromJS
+    EnumSet<Direction> getFEOutputSet();
+}
