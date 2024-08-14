0e8b6d4ccdd6b383ae499fcf4d81a10ee2e45b26
9thCore <38700349+9thCore@users.noreply.github.com>
2024-08-14 11:28:36 +0300

    add fluid input interface

---
+++ src/main/java/com/core/kubejselectrodynamics/block/capabilities/IElectrodynamicsFluidInput.java
@@ -0,0 +1,33 @@
+package com.core.kubejselectrodynamics.block.capabilities;
+
+import dev.latvian.mods.kubejs.block.BlockBuilder;
+import dev.latvian.mods.kubejs.typings.Info;
+import dev.latvian.mods.rhino.util.HideFromJS;
+import net.minecraft.core.Direction;
+
+import java.util.Arrays;
+import java.util.EnumSet;
+
+public interface IElectrodynamicsFluidInput<T extends BlockBuilder> extends ISelfAccessor<T> {
+    @Info("Sets the machine's fluid input faces.\nDirection.ALL is not supported, use #allFluidInput() instead.")
+    default T fluidInputs(Direction[] directions) {
+        getFluidInputSet().clear();
+        getFluidInputSet().addAll(Arrays.asList(directions));
+        return getThis();
+    }
+    @Info("Sets the machine's fluid input face.\nDirection.ALL is not supported, use #allFluidInput() instead.")
+    default T fluidInput(Direction direction) {
+        return fluidInputs(new Direction[] {direction});
+    }
+    @Info("Allows fluid input through every face.")
+    default T allFluidInput() {
+        getFluidInputSet().clear();
+        return getThis();
+    }
+    @HideFromJS
+    default Direction[] getFluidInput() {
+        return getFluidInputSet().toArray(new Direction[0]);
+    }
+    @HideFromJS
+    EnumSet<Direction> getFluidInputSet();
+}
