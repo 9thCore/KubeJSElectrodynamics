27eeff6af8780313d6cfab51c7c1acb9af4a78ba
9thCore <38700349+9thCore@users.noreply.github.com>
2024-08-22 21:32:26 +0300

    generalise ISelfAccessor<T>

--- src/main/java/com/core/kubejselectrodynamics/block/capabilities/ISelfAccessor.java
+++ src/main/java/com/core/kubejselectrodynamics/block/capabilities/ISelfAccessor.java
@@ -1,13 +1,13 @@
 package com.core.kubejselectrodynamics.block.capabilities;
 
-import dev.latvian.mods.kubejs.block.BlockBuilder;
+import dev.latvian.mods.kubejs.registry.BuilderBase;
 import dev.latvian.mods.rhino.util.HideFromJS;
 
 /**
  * Helper interface that several other interfaces extend to access the builder object.
  * Only requires one override for all interfaces to function.
  */
-public interface ISelfAccessor<T extends BlockBuilder> {
+public interface ISelfAccessor<T extends BuilderBase<?>> {
     @HideFromJS
     T getThis();
 }
