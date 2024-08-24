bfdfe4d0e7c05273dc736bd943787012467025c0
9thCore <38700349+9thCore@users.noreply.github.com>
2024-08-24 13:41:31 +0300

    remove unused shadow

--- src/main/java/com/core/kubejselectrodynamics/mixin/electrodynamics/event/CombustionFuelRegisterMixin.java
+++ src/main/java/com/core/kubejselectrodynamics/mixin/electrodynamics/event/CombustionFuelRegisterMixin.java
@@ -3,22 +3,15 @@ package com.core.kubejselectrodynamics.mixin.electrodynamics.event;
 import com.core.kubejselectrodynamics.plugin.event.ElectrodynamicsEvents;
 import com.core.kubejselectrodynamics.plugin.event.server.CombustionEventJS;
 import electrodynamics.common.reloadlistener.CombustionFuelRegister;
-import electrodynamics.prefab.utilities.object.CombustionFuelSource;
 import net.minecraftforge.event.OnDatapackSyncEvent;
 import net.minecraftforge.network.simple.SimpleChannel;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.HashSet;
-
 @Mixin(CombustionFuelRegister.class)
 public class CombustionFuelRegisterMixin {
-    @Shadow @Final private HashSet<CombustionFuelSource> fuels;
-
     @Inject(
             method = "lambda$getDatapackSyncListener$1",
             at = @At("HEAD"),
