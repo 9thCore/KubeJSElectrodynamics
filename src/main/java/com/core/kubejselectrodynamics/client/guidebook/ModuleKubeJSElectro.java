deefe913b4b86bf5a418b56f39d82c7158f8fe0c
9thCore <38700349+9thCore@users.noreply.github.com>
2024-08-31 09:53:36 +0300

    add gas insulation chapter

--- src/main/java/com/core/kubejselectrodynamics/client/guidebook/ModuleKubeJSElectro.java
+++ src/main/java/com/core/kubejselectrodynamics/client/guidebook/ModuleKubeJSElectro.java
@@ -1,10 +1,7 @@
 package com.core.kubejselectrodynamics.client.guidebook;
 
 import com.core.kubejselectrodynamics.KubeJSElectrodynamics;
-import com.core.kubejselectrodynamics.client.guidebook.chapters.ChapterFluidPipe;
-import com.core.kubejselectrodynamics.client.guidebook.chapters.ChapterFuelRod;
-import com.core.kubejselectrodynamics.client.guidebook.chapters.ChapterGas;
-import com.core.kubejselectrodynamics.client.guidebook.chapters.ChapterGasPipe;
+import com.core.kubejselectrodynamics.client.guidebook.chapters.*;
 import com.core.kubejselectrodynamics.plugin.KubePlugin;
 import com.core.kubejselectrodynamics.util.TextUtil;
 import electrodynamics.client.guidebook.utils.components.Module;
@@ -28,6 +25,10 @@ public class ModuleKubeJSElectro extends Module {
 
         if (ChapterFluidPipe.valid()) {
             chapters.add(new ChapterFluidPipe(this));
+        }
+
+        if (ChapterGasInsulation.valid()) {
+            chapters.add(new ChapterGasInsulation(this));
         }
 
         if (KubePlugin.ModHolder.NUCLEARSCIENCE.valid() && ChapterFuelRod.valid()) {
---
+++ src/main/java/com/core/kubejselectrodynamics/client/guidebook/chapters/ChapterGasInsulation.java
@@ -0,0 +1,53 @@
+package com.core.kubejselectrodynamics.client.guidebook.chapters;
+
+import com.core.kubejselectrodynamics.KubeJSElectrodynamics;
+import com.core.kubejselectrodynamics.util.InsulationUtils;
+import com.core.kubejselectrodynamics.util.TextUtil;
+import electrodynamics.api.electricity.formatting.ChatFormatter;
+import electrodynamics.api.electricity.formatting.DisplayUnit;
+import electrodynamics.client.guidebook.utils.components.Chapter;
+import electrodynamics.client.guidebook.utils.components.Module;
+import electrodynamics.client.guidebook.utils.pagedata.graphics.AbstractGraphicWrapper;
+import electrodynamics.client.guidebook.utils.pagedata.graphics.ImageWrapperObject;
+import electrodynamics.client.guidebook.utils.pagedata.text.TextWrapperObject;
+import electrodynamics.prefab.utilities.ElectroTextUtils;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+
+import java.util.Map;
+
+public class ChapterGasInsulation extends Chapter {
+    private static final ImageWrapperObject LOGO = new ImageWrapperObject(0, 0, 0, 0, 32, 32, 32, 32, new ResourceLocation(KubeJSElectrodynamics.MODID, "textures/guidebook/chapter/gasinsulation/logo.png"));
+
+    public ChapterGasInsulation(Module module) {
+        super(module);
+    }
+
+    @Override
+    public void addData() {
+        pageData.add(new TextWrapperObject(TextUtil.guidebookGas("insulation.list")).setSeparateStart());
+
+        for (Map.Entry<Item, Double> entry : InsulationUtils.insulationEffectiveness.entrySet()) {
+            blankLine();
+            double percentage = (entry.getValue() - 1) * 100;
+            MutableComponent value = ChatFormatter.formatDecimals(percentage, 1);
+            pageData.add(new TextWrapperObject(TextUtil.guidebookGas("insulation.format", entry.getKey().getDescription(), value.append("%"))).setSeparateStart());
+        }
+    }
+
+    @Override
+    public AbstractGraphicWrapper<?> getLogo() {
+        return LOGO;
+    }
+
+    @Override
+    public MutableComponent getTitle() {
+        return TextUtil.guidebookGas("insulation.title");
+    }
+
+    public static boolean valid() {
+        return !InsulationUtils.insulationEffectiveness.isEmpty();
+    }
+}
